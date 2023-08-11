4e3001bdd0c2843a818034e6c4a3b53538fe60c3
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-11 09:16:00 +0500

    values

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,25 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    return 
+    find=0
+    for i in data:
+        if i['age']=='age':
+            find+=1
+    return find
+
+data=[
+    {
+        'name':'John',
+        'age':27
+    },
+    {
+        'name':'Ann',
+        'age':42
+    },
+    {
+        'name':'Mary',
+        'age':27
+    }
+  ]
+data1=data,('age',27)
+print(find_int_keys(data1))
