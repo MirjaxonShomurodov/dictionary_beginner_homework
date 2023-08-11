7a82ed00b47ff53a791bece09ca1f66f520cca88
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-11 09:17:13 +0500

    find_length

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -1,9 +1,22 @@
-def find_length_of_values(data: dict) -> int:
-    """
-    Return the sum of the length of all values in a dictionary.
-    Args:
-        data (dict): A dictionary of values
-    Returns:
-        int: The sum of the length of all values in the dictionary.
-    """
-    return 
+# def find_length_of_values(data: dict) -> int:
+#     """
+#     Return the sum of the length of all values in a dictionary.
+#     Args:
+#         data (dict): A dictionary of values
+#     Returns:
+#         int: The sum of the length of all values in the dictionary.
+#     """
+
+data = {
+    1 : "Khiva", 
+    2 : "Namangan", 
+    3 : "Samarkand", 
+    4 : "Tashkent"
+  }
+arr=len(data[1])
+arr1=len(data[2])
+arr2=len(data[3])
+arr3=len(data[4])
+arr4=arr+arr1+arr2+arr3
+print(arr4)
+    
