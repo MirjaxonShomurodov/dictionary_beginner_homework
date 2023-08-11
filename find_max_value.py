86383c6cf6d965f3ef256bf2eb33a4866491051b
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-11 09:10:04 +0500

    value

--- find_max_value.py
+++ find_max_value.py
@@ -6,4 +6,21 @@ def find_max_value(data: dict):
     Returns:
         int: The maximum value in the dictionary.
     """
-    return 
+    data = {
+        'a' : -4, 
+        'b' : -10, 
+        'c' : 0
+         }
+    
+    arr=(data['a'])
+    arr1=(data['b'])
+    arr2=(data['c'])
+
+    if arr>arr1 and arr>arr2:
+        return arr
+    if arr1>arr and arr1>arr2:
+        return arr1
+    if arr2>arr and arr2>arr1:
+        return arr2
+
+print(find_max_value("data"))
