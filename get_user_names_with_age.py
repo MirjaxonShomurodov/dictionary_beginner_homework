fab36cfeb26ff40bd49b5bb91e293a9785469934
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-15 05:45:53 +0500

    wiht

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,33 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    names=[]
+    while data:
+        user = data.pop()
+        user_age=user['age']
+        name = user['name']
+        
+        if age == user_age:
+            names.append(name)
+        return name
+
+data = [
+            {
+            'name': 'John', 
+            'age': 30
+            }, 
+            {
+            'name': 'Ann', 
+            'age': 32
+            }, 
+            {
+            'name': 'Sam', 
+            'age': 27
+            }, 
+            {
+            'name': 'Mary', 
+            'age': 32
+            }
+        ]
+print(get_user_names_with_age(data,30))
+
